07204326efae49aaef875fb66a1affcf6e8f52ca
Arthur José <168224092+ArthurJsph@users.noreply.github.com>
2024-05-08 14:47:52 -0300

    Update ContaTerminal.java
    Não consegui fazer com que a alteração feita no vs code viesse pro repositorio remoto

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
+        Scanner leia = new Scanner(System.in);
+        int numero;
+        String agencia, nomeCliente;
+        float saldo;
+        System.out.println("Por favor, digite o número da Agência !");
+        numero = leia.nextInt();
+        leia.nextLine();
+        System.out.println("Por favor, digite sua agência!");
+        agencia = leia.nextLine();
+        System.out.println("Por favor, digite o seu nome!");
+        nomeCliente = leia.nextLine();
+        System.out.println("Por favor, digite seu saldo!");
+        saldo = leia.nextFloat();
+        System.out.println("Olá" + nomeCliente + ",obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
